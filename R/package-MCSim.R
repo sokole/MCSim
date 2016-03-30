c2436558a445d651dd9d35edec4a328c52aefc7c
sokole <sokole@gmail.com>
2016-03-29 19:28:15 -0600

    correct misspellinggggg!!!

--- R/package-MCSim.R
+++ R/package-MCSim.R
@@ -1,6 +1,6 @@
 #' MCSim: A metacommunity simulation package
 #' 
-#' The MCSim package provides functinos necessary for running iterative, lottery-based metacommunity simulations
+#' The MCSim package provides functions necessary for running iterative, lottery-based metacommunity simulations
 #' 
 #' @docType package
 #' @name MCSim
