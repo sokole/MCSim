e77783cc291b8f9af9990c42d6fb0b7af8a18626
sokole <sokole@gmail.com>
2016-03-29 19:09:52 -0600

    add package documentation file

---
+++ R/package-MCSim.R
@@ -0,0 +1,7 @@
+#' MCSim: A metacommunity simulation package
+#' 
+#' The MCSim package provides functinos necessary for running iterative, lottery-based metacommunity simulations
+#' 
+#' @docType package
+#' @name MCSim
+NULL
