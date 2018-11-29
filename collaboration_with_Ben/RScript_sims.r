1fac9cd64dca8b98b46e350d8f3c85db68d34ce6
Eric R. Sokol <esokol@battelleecology.org>
2018-11-28 17:20:43 -0700

    updated neutral scenario

--- collaboration_with_Ben/RScript_sims.r
+++ collaboration_with_Ben/RScript_sims.r
@@ -16,15 +16,58 @@ library(MCSim)
 
 library(tidyverse)
 
+my_scenario_name <- 'neutral_unlimited_dispersal'
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # landscape design common to all sims -- two sets of clumped sites
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~
+set.seed(123)
+
 # generating pre and post disturbance landscapes
 xy_coordinates <- data.frame(
-  x = c(1, 1.5, 3, 4, 5),
-  y = c(1, 1.5, 1, 6, 2))
+  x = c(1, 2, 3, 8, 9, 10),
+  y = c(1, 2, 1, 8, 9, 8))
 
 plot(xy_coordinates)
 
+#############
+# send function landscape list and interval duration list, along with other metaSim parameters that will be fixed across all simulations
+n_sites <- nrow(xy_coordinates)
 
+my_landscape <- MCSim::fn.make.landscape( # pre-disturbance landscape
+  site.coords = xy_coordinates,
+  m = rep(.1, n_sites),
+  Ef = runif(n_sites, -1, 1),
+  JL = rep(100, n_sites))
+
+
+# -- fixed across simulations
+# niche positions, niche breadths, and relative abundances for 6 species
+my_regional_rel_abund <- c(9, 2, 1, 1)
+my_regional_rel_abund <- my_regional_rel_abund / sum(my_regional_rel_abund)
+
+n_spp <- length(my_regional_rel_abund)
+my_niche_positions <-  rep(.1, n_spp)
+my_niche_breadths <- rep(.1, n_spp)
+
+
+# -- call the wrapper function
+my_sim_result <- fn.metaSIM(
+  scenario.ID = my_scenario_name,
+  landscape = my_landscape,
+  gamma.abund = my_regional_rel_abund,
+  trait.Ef = my_niche_positions,
+  trait.Ef.sd = my_niche_breadths,
+  W.r = 0,
+  nu = 0.001,
+  save.sim = FALSE
+)
+
+data_wide <- my_sim_result$J.long %>% group_by(timestep, site) %>% spread(spp, count)
+print(data_wide)
+
+
+my_sim_result$J.long %>% ggplot(aes(timestep, count, color = spp)) +
+  geom_line() +
+  facet_wrap(~ as.factor(site))
 
