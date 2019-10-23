a5b2db47c7fa4cb50f088fabeb7c86697c610e0d
Eric R. Sokol <sokole@gmail.com>
2019-10-23 08:58:21 -0600

    update namespace for pipe, fix bugs with plot_coenoclines function

--- R/plot_coenoclines.r
+++ R/plot_coenoclines.r
@@ -7,7 +7,7 @@
 #' @param Ef numeric vector of environmental filter values for sites in a simulation result, default is NULL
 #' @param trait_Ef numeric vector of niche positions of species in metacommunity, default is NULL
 #' @param trait_Ef_sd numeric vector of niche breadths, default is NULL
-#' 
+#' @param plot_trait_Ef_sd_0s should species with niche-breadth of 0 be plotted? Default is FALSE
 #' 
 #' 
 #' @export
@@ -17,6 +17,7 @@ plot_coenoclines <- function(
     Ef = NULL,
     trait_Ef = NULL,
     trait_Ef_sd = NULL,
+    plot_trait_Ef_sd_0s = FALSE,
     ...){
     
   requireNamespace("magrittr")
@@ -56,18 +57,22 @@ plot_coenoclines <- function(
       (1/sigma * sqrt(2 * pi)) * exp((-1  *(x - mu)^2) / (2 * sigma^2)), ...) #formula for bell curve
   }
 
-  # # get y_max_val
+  # # get y_max_val, minimum is 1
   y_max_val <- 1
-
   for (i.spp in 1:n_spp){
-    max_val_tmp <- (fn_norm_curve(
-      mu = trait_Ef[i.spp], 
-      sigma = trait_Ef_sd[i.spp], 
-      add = FALSE))$y %>% max()
-    y_max_val <- max(c(y_max_val, max_val_tmp), na.rm = TRUE)
+    if(trait_Ef_sd[i.spp] > 0){
+      max_val_tmp <- (fn_norm_curve(
+        mu = trait_Ef[i.spp], 
+        sigma = trait_Ef_sd[i.spp],
+        from = min(Ef),
+        to = max(Ef),
+        add = FALSE))$y %>% max()
+      y_max_val <- max(c(y_max_val, max_val_tmp), na.rm = TRUE)
+    }
   }
 
   # -- Initialize plot of coenoclines
+
   plot(1,1,
        xlim = c(min(Ef), max(Ef)),
        ylim = c(0, y_max_val),
@@ -76,16 +81,22 @@ plot_coenoclines <- function(
        ylab = 'Prob. dens.',
        main = 'Niche positions and widths',
        ...)
-
+  
   mypal <- rainbow(n_spp)
-
+  
   # -- loop to plot each species' habitat preference
   for (i.spp in 1:n_spp){
-    fn_norm_curve(
-      mu = trait_Ef[i.spp], 
-      sigma = trait_Ef_sd[i.spp], 
-      add = TRUE,
-      col = mypal[i.spp])
+    if(trait_Ef_sd[i.spp] > 0){
+      fn_norm_curve(
+        mu = trait_Ef[i.spp], 
+        sigma = trait_Ef_sd[i.spp], 
+        add = TRUE,
+        col = mypal[i.spp])
+    }else if(plot_trait_Ef_sd_0s & trait_Ef_sd[i.spp] == 0){
+      # plot verticle lines when trait_Ef_sd == 0, if desired
+      abline(v = trait_Ef[i.spp],
+             col = mypal[i.spp])
+    }
   }
 
   # -- plot sites along the x-axis
